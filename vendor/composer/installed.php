798693a27b66cfdcea3fe3311691b00886f00a04
unknown <pavlovicnikola511@gmail.com>
2020-11-15 07:43:17 +0100

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e84570ed36e1429376185ffdb18dc3e69816e8f4',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e84570ed36e1429376185ffdb18dc3e69816e8f4',
+    ),
+  ),
+);
