a59cb8dedd89ad868eaa91c01e33121385e209e7
unknown <pavlovicnikola511@gmail.com>
2020-11-15 08:46:18 +0100

    remote sql

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -1,9 +1,16 @@
 <?php
 
-$serverName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "pegas";
+// Production Connection
+$serverName = "remotemysql.com";
+$dbName = "7T7edyOVpr";
+$dbUsername = "7T7edyOVpr";
+$dbPassword = "ZY4HFpDtWb";
+
+// Development Connection
+// $serverName = "localhost";
+// $dbName = "pegas";
+// $dbUsername = "root";
+// $dbPassword = "";
 
 $conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
 
